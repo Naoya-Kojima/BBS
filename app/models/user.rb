1ed8eec97af5b7a46d5d3d1fd2cca6b02095982b
Kojima Naoya <naoyak1989@gmail.com>
2019-04-19 21:12:57 +0900

    userモデルの修正 #8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  has_secure_password
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password, confirmation: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 4 }
   validates :password_confirmation, presence: true
   validates :sex, presence: true
   enum sex: { male: 0, female: 1 }
