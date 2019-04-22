97d4108b7db80644f7f7b93b73bf06c017801631
Kojima Naoya <naoyak1989@gmail.com>
2019-04-22 11:34:25 +0900

    不要なmigrateファイルを削除 #8

--- db/migrate/20190419072715_add_password_digest_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordDigestUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :crypted_password, :string
-    add_column :users, :password_digest, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_072715) do
+ActiveRecord::Schema.define(version: 2019_04_19_044349) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 2019_04_19_072715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
-    t.string "salt"
     t.string "password_digest"
+    t.string "salt"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
