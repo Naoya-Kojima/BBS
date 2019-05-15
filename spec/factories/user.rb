a624ef78bb20799da4458161e4b91a74e2a567dc
Kojima Naoya <naoyak1989@gmail.com>
2019-05-15 19:33:32 +0900

    factories/userの修正、user_specの余計な記述を削除 #27

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :user do
     name                  { Faker::Name.name }
-    email                 { Faker::Internet.email }
+    email                 { Faker::Internet.safe_email }
     password              { 'password1234' }
     password_confirmation { 'password1234' }
-    salt                  { 'asdasdastr4325234324sdfds' }
     sex                   { 0 }
   end
 end
